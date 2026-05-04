47dbe734b5b5f40481f025634d967cab29c37368
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2026-05-04 21:32:39 +0530

    Day-1 Intro)

---
+++ Day-1 (Intro)/demo.c
@@ -0,0 +1,10 @@
+//Introduction of C Lang and Basic Syntax
+
+#include<stdio.h> // header file (stdio= standard input output, # = preprocessor)
+
+int main(){ // 2 type of return type- 1)void = null 2)int = integers number values(0)
+
+    printf("Hello C learners! \n"); // "\n = next line"
+    printf("Hello Mam!");
+    return 0;
+}
