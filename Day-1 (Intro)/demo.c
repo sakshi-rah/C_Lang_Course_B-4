1e10f7ff700251c9e6c885bf4af3c5cc58719f43
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2026-05-06 22:00:23 +0530

    Day-3 (Operators-1-Arithmetic Operators)

--- Day-1 (Intro)/demo.c
+++ Day-1 (Intro)/demo.c
@@ -4,7 +4,19 @@
 
 int main(){ // 2 type of return type- 1)void = null 2)int = integers number values(0)
 
-    printf("Hello C learners! \n"); // "\n = next line"
-    printf("Hello Mam!");
+    // printf("Hello C learners! \n"); // "\n = next line"
+    // printf("Hello Mam!");
+
+    char name[20];
+    int age;
+    printf("enter name: ");
+    scanf("%s", &name);
+    printf("Name: %s\n", name);
+
+    printf("enter age: ");
+    scanf("%d", &age);
+    printf("Age: %d", age);
+
+
     return 0;
 }
